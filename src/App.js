7ce77fbb87ee6b1c8d997ae39e8f668a6862db2d
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-05 14:38:30 -0600

    working on search bar in app file

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
   return (
     <div>
-  
+      <input type="text" className="searchTerm" placeholder='game search'></input>
+      <button type="submit" className='searchBar'>
+        <i className='fa-fa search'>Search</i> 
+      </button>
     </div>
   );
 }
