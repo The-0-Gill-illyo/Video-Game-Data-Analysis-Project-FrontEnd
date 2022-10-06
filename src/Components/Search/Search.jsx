e165c7aa888706d13fb77685744afd1a36d4881e
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-06 09:02:54 -0600

    switched search code to searchbar page

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -10,13 +10,11 @@ const Search = (props) => {
   console.log("Search Games")
     }, [])
     return ( 
-    <div>
-        <input type="text" className="searchTerm" placeholder='game search'></input>
-        <button type="submit" className='searchBar'>
-          <i className='fa-fa search'>Search</i> 
-        </button>
-    </div>
+
      );
 }
  
 export default Search;
+
+
+// let filteredGameSelection = game.filter(element => element.id == "game")
--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,8 +1,14 @@
 
 
 const SearchBar = (game) => {
-    let filteredGameSelection = game.filter(element => element.id == "game")
-    return filteredGameSelection
+    return (
+        <div>
+            <input type="text" className="searchTerm" placeholder='game search'></input>
+            <button type="submit" className='searchBar'>
+            <i className='fa-fa search'>Search</i> 
+            </button>
+        </div>
+    )
 }
 
 export default SearchBar;
