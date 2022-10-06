ac9b0256b96d580f0f87e003ea9bf2086afa159e
The-0-Gill-illyo <theogill1369@gmail.com>
2022-10-06 16:45:59 -0400

    Started record graph

--- src/Components/RecordsGraph/RecordsGraph.jsx
+++ src/Components/RecordsGraph/RecordsGraph.jsx
@@ -1,23 +1,41 @@
-import {useState, useEffect} from 'react';
-// import {Chart} from 'react-google-charts';
+import {Chart} from 'react-google-charts';
 
 const RecordsGraph = (props) => {
-    useEffect(() => {
-        displayData()
-    }, [])
 
-    function displayData(){
-        console.log("Videogame data:", props)
+    function DataTable(){
+
+        let filterData = props.filter(game => game.year >= 2013)
+
+        console.log(filterData)
+
+        const data = [
+            ["Platform", "Sales"],
+            ["2014", 1000, 400, 200],
+            ["2015", 1170, 460, 250],
+            ["2016", 660, 1120, 300],
+            ["2017", 1030, 540, 350],
+            ];
+
+            return data;
+    }
+        
 
 
+
+
+
+
+        return ( 
+        <div>
+            <Chart
+            chartType="Bar"
+            width="100%"
+            height="400px"
+            data={DataTable()}
+            />
+        </div>
+        );
+    
     }
 
-
-
-    return ( 
-        <h1>Graph Here</h1>
-
-     );
-}
- 
 export default RecordsGraph;
