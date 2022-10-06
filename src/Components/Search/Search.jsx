d4240238bee50a0885baeeefb999f077761dcaa7
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-06 09:25:37 -0600

    created handle submit functionality for search bar

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,20 +1,21 @@
 import { useEffect, useState } from "react";
+import SearchBar from "../SearchBar/SearchBar";
 
 
 
 const Search = (props) => {
-    const [searchGames, setSearchGames] = useState([]);
-    const [searchInput, setSearchInput] = useState("");
-    useEffect(() => {
-
+  const [searchGames, setSearchGames] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  useEffect(() => {
+  
   console.log("Search Games")
-    }, [])
-    return ( 
-
-     );
-}
- 
-export default Search;
-
+  }, [])
+  return ( 
+    <SearchBar />
+    );
+  }
+  
+  export default Search;
+  
 
 // let filteredGameSelection = game.filter(element => element.id == "game")
--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,14 +1,40 @@
+import React, {useEffect, useState} from 'react';
 
+const SearchBar = (props) => {
+    const [searchTerm, setSearchTerm] = useState('');
 
-const SearchBar = (game) => {
+    useEffect(()=>{
+
+    },[])
+
+    function handleSubmit(event){
+        event.preventDefault();
+        console.log(searchTerm)
+        props.searchGames(searchTerm)
+    }
+
     return (
         <div>
-            <input type="text" className="searchTerm" placeholder='game search'></input>
-            <button type="submit" className='searchBar'>
-            <i className='fa-fa search'>Search</i> 
-            </button>
+            <form onSubmit={handleSubmit}>
+                <input type="text" className="searchTerm" placeholder='game search' 
+                        value={searchTerm}
+                        onChange={(event) => setSearchTerm(event.target.value)}></input>
+                <button type="submit" className='searchBar'>
+                <i className='fa-fa search'>Search</i> 
+                </button>
+            </form>
         </div>
     )
 }
 
 export default SearchBar;
+
+
+{/* <div className="container">
+            <form onSubmit={handleSubmit}>             
+              <input type="text"  className="searchTerm" onChange={(event)=>setSearchInput(event.target.value)} placeholder="SEARCH"></input>
+              <button type='submit' className="searchButton">
+              <i className="fa-fa search">search</i>
+              </button>
+             
+            </form> */}
