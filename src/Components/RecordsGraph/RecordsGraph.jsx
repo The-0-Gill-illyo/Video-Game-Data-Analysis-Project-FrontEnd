30b9af223e5424ad30ec806091bf78017cafd175
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-07 12:18:55 -0600

    cleaned up records graph page

--- src/Components/RecordsGraph/RecordsGraph.jsx
+++ src/Components/RecordsGraph/RecordsGraph.jsx
@@ -6,48 +6,31 @@ const RecordsGraph = (props) => {
 
         let filterData = props.parentEntries.filter(game => game.year >= 2013)
 
-        // console.log("Filtered Data", filterData)
-
         let platforms = filterData.map(game => {
             return game.platform
         });
-        // console.log("Platform Data", platforms)
 
         let originalPlatform = [...new Set(platforms)]
 
-        // console.log("original platform", originalPlatform)
 
         let consoleArray = originalPlatform.map(platform => {
             let allGamesForPlatform = filterData.filter(game => game.platform == platform)
-            // console.log("platform game arrays", allGamesForPlatform)
                 allGamesForPlatform.sum = function(items, game){
                     return items.reduce(function(a, b){
                         return a + b[game];
                     }, 0);
                 }
             let totalConsoleGames = allGamesForPlatform.sum(allGamesForPlatform, 'globalsales')
-            console.log("game sales", totalConsoleGames)
-
 
             return [platform, totalConsoleGames]
         })
-            console.log(consoleArray)
 
         const data = [
             ["Platform", "Sales"],
             ...consoleArray
-            
             ];
-
             return data;
     }
-        
-
-
-
-
-
-
         return ( 
         <div>
             <Chart
@@ -58,7 +41,6 @@ const RecordsGraph = (props) => {
             />
         </div>
         );
-    
     }
 
 export default RecordsGraph;
