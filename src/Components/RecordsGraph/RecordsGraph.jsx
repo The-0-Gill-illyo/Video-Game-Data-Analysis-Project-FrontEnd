745b1a7c78906a4ed9cc8995d22c3ce2439ca6bc
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-06 18:11:44 -0600

    created functionality to add all global sales together + applied to graph

--- src/Components/RecordsGraph/RecordsGraph.jsx
+++ src/Components/RecordsGraph/RecordsGraph.jsx
@@ -6,19 +6,30 @@ const RecordsGraph = (props) => {
 
         let filterData = props.parentEntries.filter(game => game.year >= 2013)
 
-        console.log("Filtered Data", filterData)
+        // console.log("Filtered Data", filterData)
 
         let platforms = filterData.map(game => {
             return game.platform
         });
-        console.log("Platform Data", platforms)
+        // console.log("Platform Data", platforms)
 
         let originalPlatform = [...new Set(platforms)]
 
-        console.log("original platform", originalPlatform)
+        // console.log("original platform", originalPlatform)
 
         let consoleArray = originalPlatform.map(platform => {
-            return [platform, 10]
+            let allGamesForPlatform = filterData.filter(game => game.platform == platform)
+            // console.log("platform game arrays", allGamesForPlatform)
+                allGamesForPlatform.sum = function(items, game){
+                    return items.reduce(function(a, b){
+                        return a + b[game];
+                    }, 0);
+                }
+            let totalConsoleGames = allGamesForPlatform.sum(allGamesForPlatform, 'globalsales')
+            console.log("game sales", totalConsoleGames)
+
+
+            return [platform, totalConsoleGames]
         })
             console.log(consoleArray)
 
