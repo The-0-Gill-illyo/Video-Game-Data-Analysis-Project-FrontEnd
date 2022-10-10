cfafc79035032af73e17bd18b5aa0a298726509b
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-10 10:59:54 -0600

    reformatted game display in search page

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,8 +1,7 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import SearchBar from "../SearchBar/SearchBar";
 import axios from "axios";
 import Game from "../Game/Game";
-
 
 
 const Search = () => {
@@ -16,69 +15,35 @@ const Search = () => {
   fetchSearchResults()
   }, [searchInput])
 
+  // useEffect (() =>{
+  //   setShowMore()
+  // }, [showMore])
+
   function passedSearchTerms(searchTerm){
     setSearchInput(searchTerm)
   }
   const fetchSearchResults = async () => {
     let response = await axios.get("http://localhost:8080/all")
-    // console.log("GameResults:", response.data)
     setSearchGames(response.data)
     let filteredSearch = searchGames.filter(game => game.name.includes(searchInput));
-    // console.log(filteredSearch)
     setVideoGame(filteredSearch)
   }
 
 
-  // async function getGameById(id){
-  //   let response = await axios.get(`http://localhost:8080/getById/${id}`);
-  //   setGames(response.data);
-  //   console.log(response.data)
-  // }
-
       return ( 
         <div>
           <SearchBar searchGames={passedSearchTerms}/>
-          <table>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Game Rank</th>
-                <th>Platform</th>
-                <th>Year</th>
-                <th>Genre</th>
-                <th>Publisher</th>
-                <th>North American Sales</th>
-                <th>Europe Sales</th>
-                <th>Japan Sales</th>
-                <th>Other Sales</th>
-                <th>Global Sales</th>
-              </tr>
-            </thead>
-            <tbody>
-              {videoGame.map((game, index) => {
+          {videoGame.map((game, index) => {
                 return(
-                  <tr key={index}>
-                    <td>{game.name}</td>
-                    <td>{game.game_rank}</td>
-                    <td>{game.platform}</td>
-                    <td>{game.year}</td>
-                    <td>{game.genre}</td>
-                    <td>{game.publisher}</td>
-                    <td>{game.northamericasales}</td>
-                    <td>{game.europesales}</td>
-                    <td>{game.japansales}</td>
-                    <td>{game.othersales}</td>
-                    <td>{game.globalsales}</td>
+                  <div key={index}>
+                    <h3>{game.name}</h3>
                     <div>
-                      {showMore ? <Game parentEntries={games}/> : ''}
+                      {showMore ? <Game parentEntries={game}/> : ''}
                       <button onClick={()=> setShowMore(!showMore)} >See More</button>
                     </div>
-                  </tr>
+                  </div>
                 )
               })}
-             
-            </tbody>
-          </table>
         </div>
         );
         
